025f396eb8af2d63dfc39afed1dfb56238784bcc
sam joseph <74926097+samjozff@users.noreply.github.com>
2020-11-24 22:31:29 +0530

    python internship day 2

---
+++ TASK 2.py
@@ -0,0 +1,25 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input("a :"))
+a :20
+>>> b=int(input("b :"))
+b :10
+>>> c=int(input("c :"))
+c :40
+>>> x=a/10
+>>> print(x)
+2.0
+>>> y=b*50
+>>> print(y)
+500
+>>> z=c+60
+>>> print(z)
+100
+>>> str="joseph"
+>>> print(str.replace('s','g'))
+jogeph
+>>> a=3
+>>> b=4.33
+>>> print(float(a),int(b))
+3.0 4
+>>> 
